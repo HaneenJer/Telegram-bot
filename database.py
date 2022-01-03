d919cd229565feae709900dd145af89c5a9b2bd8
HaneenJer <haneenjeries65@gmail.com>
2022-01-03 21:51:20 +0200

    add user and delete user works
    TODO: fix warning

--- database.py
+++ database.py
@@ -5,11 +5,30 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    chat_id = db.Column(db.Integer, primary_key=True)
 
-    def __repr__(self):
-        return '<user%r>' % self.username
+
+def db_add_usr(chat_id):
+    try:
+        user = User(chat_id=chat_id)
+        db.session.add(user)
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        return -1
+
+
+def db_delete_usr(chat_id):
+    try:
+        user = User.query.filter_by(chat_id=chat_id).first()
+        if user is not None:
+            db.session.delete(user)
+            db.session.commit()
+        else:
+            return -1
+    except Exception:
+        db.session.rollback()
+        return -1
 
 
 def create_db(app):
--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,14 @@
 from flask import *
-from database import create_db
-
-
+from database import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/tele_polls'
 app.config['SECRET_KEY'] = 'g34jdk9018220dd'
+
+NOTFOUND = 404
+CONFLICT = 409
+OK = 200
+
 
 
 @app.before_first_request
@@ -13,17 +16,22 @@ def init():
     with app.app_context():
         create_db(app)
 
+
 @app.route('/register', methods=['POST'])
 def add_user():
-    # TODO: add user to the database
-    return Response("[CHECKING]---add user")
+    status = db_add_usr(chat_id=int(request.args.get("chat_id")))
+    if status == -1:
+        return Response("this user is already registered", status=CONFLICT)
+    return Response("[CHECKING]---add user", status=OK)
     pass
 
 
 @app.route('/remove', methods=['DELETE'])
 def delete_user():
-    # TODO: remove user from the database
-    return Response("[CHECKING]---remove user")
+    status = db_delete_usr(chat_id=int(request.args.get("chat_id")))
+    if status == -1:
+        return Response("this user doesnt exist", status=NOTFOUND)
+    return Response("[CHECKING]---remove user", status=OK)
     pass
 
 
