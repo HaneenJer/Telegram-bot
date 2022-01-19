dd49a05768c9196899990565fc5105d4f67b29a7
HaneenJer <haneenjeries65@gmail.com>
2022-01-19 09:48:26 +0200

    Update run_project.py

--- run_project.py
+++ run_project.py
@@ -1,23 +1,27 @@
+from subprocess import Popen
 import os
-import subprocess
+from time import sleep
 
 
 def run():
     os.environ['WERKZEUG_RUN_MAIN'] = 'true'
 
     "execute child programs in a new process"
-    try:
-        init_command = 'conda activate Telegram-bot'
-        init_bot = subprocess.Popen(f'{init_command}  && python main.py', shell=True)
-        init_flask = subprocess.Popen(f'{init_command}  && python server/flask_app.py', shell=True)
-        npm_run_dev = subprocess.Popen('npm run dev --prefix ./', shell=True)
-        npm_start = subprocess.Popen('npm start --prefix ./', shell=True)
+    init_command = 'conda activate Telegram-bot'
+    init_bot = Popen(f'{init_command}  && python main.py', shell=True)
+    init_flask = Popen(f'{init_command}  && python server/flask_app.py', shell=True)
+    npm_run_dev = Popen('npm run dev --prefix ./', shell=True)
+    npm_start = Popen('npm start --prefix ./', shell=True)
 
+    try:
         while True:
             init_bot.poll()
             init_flask.poll()
             npm_run_dev.poll()
             npm_start.poll()
+
+            "without sleep we get error"
+            sleep(30)
 
     except Exception:
         init_bot.kill()
